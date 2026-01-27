5ea17e20868a0ebf3600f1b48f0d71c5823f114d
𝐒𝐮𝐛𝐡𝐚𝐣𝐢𝐭 𝐆𝐡𝐨𝐬𝐡 <86417238+Su6666666@users.noreply.github.com>
2026-01-27 10:41:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,25 +12,31 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 # Bot settings
 PORT = environ.get("PORT", "8080")
 
-# Online Stream and Download
+# Stream & Download
 MULTI_CLIENT = False
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-else:
-    ON_HEROKU = False
+PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))
+
+ON_HEROKU = 'DYNO' in environ
+
 URL = environ.get("URL", "")
 
-# Admins, Channels & Users
+# Channels & Admins
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001994332079'))
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '919169586').split()]
 
-# MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://SGBACKUP14:SGBACKUP@cluster0.8ub0b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+# 🔒 FORCE SUBSCRIBE
+FORCE_SUB_CHANNEL_ID = -1001877309572
+FORCE_SUB_CHANNEL_USERNAME = "sgbackup"
+
+# Database
+DATABASE_URI = environ.get(
+    'DATABASE_URI',
+    "mongodb+srv://SGBACKUP14:SGBACKUP@cluster0.8ub0b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
 DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 
-# Shortlink Info
-SHORTLINK = bool(environ.get('SHORTLINK', False)) # Set True Or False
+# Shortlink
+SHORTLINK = bool(environ.get('SHORTLINK', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
 SHORTLINK_API = environ.get('SHORTLINK_API', '')
